cd1a6ffb25b37d7a0afbcb346bebbc1f6a562ad0
Jiannan Ye <635596704@qq.com>
2025-07-11 18:46:51 +0800

    modified

--- Assets/Scripts/ESKF.cs
+++ Assets/Scripts/ESKF.cs
@@ -1,20 +1,156 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 
-
-public class ESKF : MonoBehaviour
+[StructLayout(LayoutKind.Sequential, Pack = 4)]
+public struct NativeVector3
 {
-    // Start is called before the first frame update
-    void Start()
+    public float x, y, z;
+    public Vector3 ToUnityVector() => new Vector3(x, y, z);
+    public NativeVector3(float x, float y, float z)
     {
-        
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 4)]
+public struct NativeQuaternion
+{
+    public float qx, qy, qz, qw;
+    public Quaternion ToUnityQuaternion() => new Quaternion(qx, qy, qz, qw);
+    public NativeQuaternion(float qx, float qy, float qz, float qw)
+    {
+        this.qx = qx;
+        this.qy = qy;
+        this.qz = qz;
+        this.qw = qw;
+    }
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 4)]
+public struct NativeInitData
+{
+    public float gravity;
+    public float initPosX, initPosY, initPosZ;
+    public float initVelX, initVelY, initVelZ;
+    public float initQuatX, initQuatY, initQuatZ, initQuatW;
+    public float initAccBiasX, initAccBiasY, initAccBiasZ;
+    public float initGyroBiasX, initGyroBiasY, initGyroBiasZ;
+    public float sigma_init_pos;
+    public float sigma_init_vel;
+    public float sigma_init_dtheta;
+    public float sigma_init_accel_bias;
+    public float sigma_init_gyro_bias;
+    public float sigma_accel;
+    public float sigma_gyro;
+    public float sigma_accel_drift;
+    public float sigma_gyro_drift;
+
+    public NativeInitData(float _gravity, Vector3 initPos, Vector3 initVel, Quaternion initQuat, Vector3 accelBias, Vector3 gyroBias,
+        float _sigma_init_pos, float _sigma_init_vel, float _sigma_init_dtheta, float _sigma_init_accel_bias, float _sigma_init_gyro_bias,
+        float _sigma_accel, float _sigma_gyro, float _sigma_accel_drift, float _sigma_gyro_drift)
+    {
+        gravity = _gravity;
+        initPosX = initPos.x;
+        initPosY = initPos.y;
+        initPosZ = initPos.z;
+        initVelX = initVel.x;
+        initVelY = initVel.y;
+        initVelZ = initVel.z;
+        initQuatX = initQuat.x;
+        initQuatY = initQuat.y;
+        initQuatZ = initQuat.z;
+        initQuatW = initQuat.w;
+        initAccBiasX = accelBias.x;
+        initAccBiasY = accelBias.y;
+        initAccBiasZ = accelBias.z;
+        initGyroBiasX = gyroBias.x;
+        initGyroBiasY = gyroBias.y;
+        initGyroBiasZ = gyroBias.z;
+        sigma_init_pos = _sigma_init_pos;
+        sigma_init_vel = _sigma_init_vel;
+        sigma_init_dtheta = _sigma_init_dtheta;
+        sigma_init_accel_bias = _sigma_init_accel_bias;
+        sigma_init_gyro_bias = _sigma_init_gyro_bias;
+        sigma_accel = _sigma_accel;
+        sigma_gyro = _sigma_gyro;
+        sigma_accel_drift = _sigma_accel_drift;
+        sigma_gyro_drift = _sigma_gyro_drift;
+    }
+}
+public static class ESKFNativeMethods
+{
+    const string DLL_NAME = "eskf_android_library";
+
+    [DllImport(DLL_NAME)] public static extern float getTrace();
+    [DllImport(DLL_NAME)] public static extern IntPtr CreateESKFClass(ref NativeInitData initData);
+    [DllImport(DLL_NAME)] public static extern IntPtr GetPos(IntPtr obj);
+    [DllImport(DLL_NAME)] public static extern IntPtr GetQuat(IntPtr obj);
+    [DllImport(DLL_NAME)] public static extern void FreeVector(IntPtr obj);
+    [DllImport(DLL_NAME)] public static extern void FreeQuaternion(IntPtr obj);
+    [DllImport(DLL_NAME)] public static extern NativeVector3 TestDataInOut(NativeVector3 vec);
+    [DllImport(DLL_NAME)] public static extern void PredictIMU(IntPtr obj, NativeVector3 a_m, NativeVector3 omega_m, float dt);
+}
+public class ESKF : IDisposable
+{
+
+    private IntPtr _nativePtr;
+
+    public ESKF(float gravity, Vector3 initPos, Vector3 initVel, Quaternion initQuat, Vector3 accelBias, Vector3 gyroBias, 
+        float sigma_init_pos, float sigma_init_vel, float sigma_init_dtheta, float sigma_init_accel_bias, float sigma_init_gyro_bias,
+        float sigma_accel, float sigma_gyro, float sigma_accel_drift, float sigma_gyro_drift)
+    {
+        NativeInitData initData = new NativeInitData (gravity, initPos, initVel, initQuat,
+            accelBias, gyroBias, sigma_init_pos, sigma_init_vel, sigma_init_dtheta,
+            sigma_init_accel_bias, sigma_init_gyro_bias, sigma_accel, sigma_gyro, sigma_accel_drift, sigma_gyro_drift);
+
+        _nativePtr = ESKFNativeMethods.CreateESKFClass(ref initData);
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 GetPos()
     {
-        
+        IntPtr vecPtr = ESKFNativeMethods.GetPos(_nativePtr);
+        NativeVector3 nativeVec = Marshal.PtrToStructure<NativeVector3>(vecPtr);
+        Vector3 res = nativeVec.ToUnityVector();
+        ESKFNativeMethods.FreeVector(vecPtr);
+        return res;
     }
+
+    public Quaternion GetQuat()
+    {
+        IntPtr quatPtr = ESKFNativeMethods.GetQuat(_nativePtr);
+        NativeQuaternion nativeQuat = Marshal.PtrToStructure<NativeQuaternion>(quatPtr);
+        Quaternion res = nativeQuat.ToUnityQuaternion();
+        ESKFNativeMethods.FreeQuaternion(quatPtr);
+        return res;
+    }
+
+    public void PredictIMU(Vector3 a_m, Vector3 omega_m, float dt)
+    {
+        NativeVector3 n_a_m = new NativeVector3(a_m.x, a_m.y, a_m.z);
+        NativeVector3 n_omega_m = new NativeVector3(omega_m.x, omega_m.y, omega_m.z);
+        ESKFNativeMethods.PredictIMU(_nativePtr, n_a_m, n_omega_m, dt);
+    }
+
+    static public Vector3 TestDataInOut(Vector3 inVec)
+    {
+        NativeVector3 back = ESKFNativeMethods.TestDataInOut(new NativeVector3(inVec.x, inVec.y, inVec.z));
+        return back.ToUnityVector();
+    }
+
+    public void Dispose()
+    {
+        if (_nativePtr != IntPtr.Zero)
+        {
+            _nativePtr = IntPtr.Zero;
+        }
+        GC.SuppressFinalize(this);
+    }
+
+    ~ESKF() { Dispose(); }
 }
--- Assets/Scripts/FileManager.cs
+++ Assets/Scripts/FileManager.cs
@@ -10,11 +10,43 @@ public class FileManager : MonoBehaviour
     private List<string> lines = new List<string>();
     private int currentLine = 0;
     public TMPro.TMP_Text textMeshPro;
+    public TMPro.TMP_Text FPS;
+    public TMPro.TMP_Text IMUPosAndRot;
     public GameObject indicator;
     public GameObject IMUIndicator;
+    private float deltaTime;
+    private ESKF eskf = null;
 
+    private float oldTime = -1f;
+    private void Awake()
+    {
+        float sigma_accel = 0.00124f; // [m/s^2]  (value derived from Noise Spectral Density in datasheet)
+        float sigma_gyro = 0.276f; // [rad/s] (value derived from Noise Spectral Density in datasheet)
+        float sigma_accel_drift = 0.001f * sigma_accel; // [m/s^2 sqrt(s)] (Educated guess, real value to be measured)
+        float sigma_gyro_drift = 0.001f * sigma_gyro; // [rad/s sqrt(s)] (Educated guess, real value to be measured)
+
+        float sigma_init_pos = 1.0f; // [m]
+        float sigma_init_vel = 0.1f; // [m/s]
+        float sigma_init_dtheta = 1.0f; // [rad]
+        float sigma_init_accel_bias = 10f * sigma_accel_drift; // [m/s^2]
+        float sigma_init_gyro_bias = 10f * sigma_gyro_drift; // [rad/s]
+
+        float sigma_mocap_pos = 0.003f; // [m]
+        float sigma_mocap_rot = 0.03f; // [rad]
+
+        eskf = new ESKF(9.812f, new Vector3(0, 0, 1), Vector3.zero, Quaternion.identity, new Vector3(-1.26f, -1.09f, -1.977f), new Vector3(0.114f, -0.01f, 0.0f),
+            sigma_init_pos, sigma_init_vel, sigma_init_dtheta, sigma_init_accel_bias, sigma_init_gyro_bias, sigma_accel, sigma_gyro, sigma_accel_drift, sigma_gyro_drift
+            );
+    }
     IEnumerator Start()
     {
+
+        // 关闭VSync（关键步骤）
+        QualitySettings.vSyncCount = 0;
+
+        // 设置目标帧率为屏幕最高刷新率
+        Application.targetFrameRate = 60;
+
         string filePath = Path.Combine(Application.streamingAssetsPath, "mixedTimeSeries.txt");
 
         // Android需用UnityWebRequest异步加载[2,5](@ref)
@@ -32,9 +64,17 @@ public class FileManager : MonoBehaviour
         }
     }
 
+    float timestamp(int sec, int nsec)
+    {
+        return (float)(sec) + nsec * 1e-9f;
+    }
     // Update is called once per frame
     void Update()
     {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f; //平滑实时帧率（FPS）显示器
+        float fps = 1.0f / deltaTime;
+        FPS.text = "FPS: " + Mathf.Ceil(fps).ToString();
+
         if (currentLine < lines.Count)
         {
             string currentStr = lines[currentLine];
@@ -52,10 +92,46 @@ public class FileManager : MonoBehaviour
                 Quaternion rot = new Quaternion(qX, qY, qZ, qW);
                 indicator.transform.position = pos;
                 indicator.transform.rotation = rot;
-                textMeshPro.text = lines[currentLine];
-                Debug.Log(lines[currentLine]); // 打印当前行
+                textMeshPro.text = lines[currentLine] + "\n" + "ESKF Readback:" + eskf.GetPos().ToString("0.0000,0.0000,0.0000") + "\n" + ESKF.TestDataInOut(pos).ToString("0.0000,0.0000,0.0000");
+
+                //Debug.Log("TestDllDataInOut:" + ESKF.TestDataInOut(pos).ToString());
+                //Debug.Log(lines[currentLine]); // 打印当前行
             }
-            
+
+            if (strFields.Length > 0 && strFields[0] == "IMU")
+            {
+                float accelX = float.Parse(strFields[1]);
+                float accelY = float.Parse(strFields[2]);
+                float accelZ = float.Parse(strFields[3]);
+                float omegaX = float.Parse(strFields[4]);
+                float omegaY = float.Parse(strFields[5]);
+                float omegaZ = float.Parse(strFields[6]);
+                Vector3 accel = new Vector3(accelX, accelY, accelZ);
+                Vector3 omega = new Vector3(omegaX, omegaY, omegaZ);
+
+                int sec = int.Parse(strFields[7]);
+                int nsec = int.Parse(strFields[8]);
+                float dt;
+                
+                float currentTime = timestamp(sec, nsec);
+                dt = currentTime - oldTime;
+                if (dt > 1999f) dt = 0.001f;
+                if (eskf is null)
+                {
+                    Debug.LogError("eskf is null");
+                    return;
+                }
+                eskf.PredictIMU(accel, omega, dt);
+                IMUPosAndRot.text = lines[currentLine] + "\n" + "ESKF Readback:" + eskf.GetPos().ToString("0.000000,0.000000,0.000000") + "\nrot:" + eskf.GetQuat().ToString() + "\n@time:" + sec + "," + nsec;
+                IMUIndicator.transform.position = eskf.GetPos();
+                IMUIndicator.transform.rotation = eskf.GetQuat();
+
+                oldTime = currentTime;
+                //Debug.Log("TestDllDataInOut:" + ESKF.TestDataInOut(pos).ToString());
+                //Debug.Log(lines[currentLine]); // 打印当前行
+            }
+
+
             currentLine++;
         }
     }
